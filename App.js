f87280ddc12246958d6b4f3b696e80f47e1c866e
connectionlab <connectionlab@connectionlabui-iMac-305.local>
2019-01-31 18:09:37 +0900

    updated mapView and user location

--- App.js
+++ App.js
@@ -12,85 +12,25 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-const SCREEN_HEIGHT = height
-const SCREEN_WIDTH = width
-const ASPECT_RATIO = width / height
-const LATTITUDE_DELTA = 0.0922
-const LONGITUDE_DELTA = LATTITUDE_DELTA * ASPECT_RATIO
 
 export default class App extends Component {
-  constructor(props){
-    super(props)
-
-    this.state = {
-      initialPosition: {
-        latitude: 0,
-        longitude: 0,
-        latitudeDelta:0,
-        longitudeDelta:0,
-      },
-      markerPosition: {
-        latitude: 0,
-        longitude:0
-      }
-    }
-  }
-
-  watchID: ?number = null
-
-  componentDidMount(){
-    navigator.geolocation.getCurrentPosition((position) => {
-      var lat = parseFloat(position.coords.latitude)
-      var long = parseFloat(position.coords.longitude)
-
-      var initialRegion = {
-        latitude: lat,
-        longitude: long,
-        latitudeDelta: LATTITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA
-      }
-
-      this.setState({initialPosition: initialRegion})
-      this.setState({markerPosition: initialRegion})
-    },
-    (error) => alert(JSON.stringify(error)),
-    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
-
-    this.watchID = navigator.geolocation.watchPosition((position) => {
-      var lat = parseFloat(position.coords.latitude)
-      var long = parseFloat(position.coords.longitude)
-
-      var lastRegion = {
-        latitude: lat,
-        longitude: long,
-        latitudeDelta: LATTITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA
-      }
-      this.setState({initialPosition: lastRegion})
-      this.setState({markerPosition: lastRegion})
-    })
-  }
-
-  componentWillUnmount(){
-    navigator.geolocation.clearWatch(this.watchID)
-  }
 
   render() {
     return (
-      <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        region={this.state.initialPosition}>
-
-        <MapView.Marker
-          coordinate={this.state.markerPosition}>
-          <View style={styles.radius}>
-            <View style={styles.marker}>
-            </View>
-          </View>
-        </MapView.Marker>
-      </MapView>
-    </View>
+      <View style={{flex:1}}>
+      <View style={{flex:1}}>
+        <MapView style={styles.map}
+        showsUserLocation
+        initialRegion={{
+      latitude: 37.523814,
+      longitude:  126.927494,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}/>
+      </View>
+      <View style={{height:80, width:'100%', backgroundColor:'gray'}}>
+      </View>
+      </View>
     );
   }
 }
@@ -102,35 +42,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  radius:{
-    height: 50,
-    width: 50,
-    borderRadius: 50/2,
-    overflow: 'hidden',
-    backgroundColor: 'rgba(0, 122, 255, 0.1)',
-    borderWidth:1,
-    borderColor: 'rgba(0, 112, 255, 0.3)',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  marker: {
-    height: 20,
-    width: 20,
-    borderWidth: 3,
-    borderColor: 'white',
-    borderRadius: 20/2,
-    overflow: 'hidden',
-    backgroundColor: '#007aff'
-  },
   container: {
    ...StyleSheet.absoluteFillObject,
-
  },
- map: {
+ infoWrapper: {
+   flex:1,
    left:0,
    right:0,
    top:0,
    bottom:0,
    position:'absolute',
  },
+ map: {
+   ...StyleSheet.absoluteFillObject,
+ }
 });
