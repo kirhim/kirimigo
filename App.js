ca980acd09bc5123dec0590401c4b9fbee6b69aa
connectionlab <connectionlab@connectionlabui-iMac-305.local>
2019-01-31 17:22:29 +0900

    maps showing

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Dimensions} from 'react-native';
 import StartScreen from './src/HowToRide/StartScreen'
 import MapView from 'react-native-maps'
 
+const { width, height } = Dimensions.get('window')
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -11,17 +12,85 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const SCREEN_HEIGHT = height
+const SCREEN_WIDTH = width
+const ASPECT_RATIO = width / height
+const LATTITUDE_DELTA = 0.0922
+const LONGITUDE_DELTA = LATTITUDE_DELTA * ASPECT_RATIO
+
 export default class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      initialPosition: {
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta:0,
+        longitudeDelta:0,
+      },
+      markerPosition: {
+        latitude: 0,
+        longitude:0
+      }
+    }
+  }
+
+  watchID: ?number = null
+
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition((position) => {
+      var lat = parseFloat(position.coords.latitude)
+      var long = parseFloat(position.coords.longitude)
+
+      var initialRegion = {
+        latitude: lat,
+        longitude: long,
+        latitudeDelta: LATTITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+
+      this.setState({initialPosition: initialRegion})
+      this.setState({markerPosition: initialRegion})
+    },
+    (error) => alert(JSON.stringify(error)),
+    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lat = parseFloat(position.coords.latitude)
+      var long = parseFloat(position.coords.longitude)
+
+      var lastRegion = {
+        latitude: lat,
+        longitude: long,
+        latitudeDelta: LATTITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+      this.setState({initialPosition: lastRegion})
+      this.setState({markerPosition: lastRegion})
+    })
+  }
+
+  componentWillUnmount(){
+    navigator.geolocation.clearWatch(this.watchID)
+  }
+
   render() {
     return (
+      <View style={styles.container}>
       <MapView
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
+        style={styles.map}
+        region={this.state.initialPosition}>
+
+        <MapView.Marker
+          coordinate={this.state.markerPosition}>
+          <View style={styles.radius}>
+            <View style={styles.marker}>
+            </View>
+          </View>
+        </MapView.Marker>
+      </MapView>
+    </View>
     );
   }
 }
@@ -33,14 +102,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  radius:{
+    height: 50,
+    width: 50,
+    borderRadius: 50/2,
+    overflow: 'hidden',
+    backgroundColor: 'rgba(0, 122, 255, 0.1)',
+    borderWidth:1,
+    borderColor: 'rgba(0, 112, 255, 0.3)',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  marker: {
+    height: 20,
+    width: 20,
+    borderWidth: 3,
+    borderColor: 'white',
+    borderRadius: 20/2,
+    overflow: 'hidden',
+    backgroundColor: '#007aff'
   },
+  container: {
+   ...StyleSheet.absoluteFillObject,
+
+ },
+ map: {
+   left:0,
+   right:0,
+   top:0,
+   bottom:0,
+   position:'absolute',
+ },
 });
